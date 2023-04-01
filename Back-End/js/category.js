c9064fd7f74d2fd6882dea8e69801f0718026fed
Đào Xuân Dương 2019604051 <daoxuanduong2001@gmail.com>
2023-04-01 09:50:58 +0700

    first commit

---
+++ Back-End/js/category.js
@@ -0,0 +1,17 @@
+
+// $(document).ready(function () {
+//     $(".btnCategory").click(function (e) {
+//         $.ajax({
+//             type: "GET",
+//             url: "http://localhost:9090/category/categories",
+//             dataType: "json",
+//             success: function (result, status, xhr) {
+//                 console.log(result);
+//             },
+//             error: function (xhr, status, error) {
+//                 alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+//             }
+//     })
+
+// })
+// })
---
+++ Front-End/js/shoppingCart.js
@@ -0,0 +1,14 @@
+
+function cal_amountProduct_plus(){
+    var temp=parseInt(document.getElementById("amount_product").value);
+    temp+=1;
+    document.getElementById("amount_product").value=temp;
+    console.log("plus");
+}
+function cal_amountProduct_minus(){
+    var temp=parseInt(document.getElementById("amount_product").value);
+    if(temp>=2){
+        temp-=1;
+    }
+    document.getElementById("amount_product").value=temp;
+}
